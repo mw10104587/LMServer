b5621c24bbdc80e660f45452ea99d60695d947ef
Kenny <kenny@kenny-VirtualBox.(none)>
2013-11-02 22:35:35 +0800

    kenny add find_neighbor

--- find_subgraph.py
+++ find_subgraph.py
@@ -8,48 +8,48 @@ class Edge:
 
 def sub_graph():
     
-	g_query = Graph()
-	g_map = Graph()
-	
-	# load user query graph
-	# load map
-	initParse()
-	g_query = fetchGraph()
-	g_map = fetchGraph() #???
-	
-	
-	# find the most suitable starting vertex
-	edges = []
-	for i, edge in enumerate(g_query.es):
-	    class_e = Edge(edge.index, edge["length"], edge["n1"], edge["n2"], edge["bearing1"], edge["bearing2"])
-	    edges.append(class_e)
-    angleArray = getAngleArrayWithEdgeArray(edges)		
+    g_query = Graph()
+    g_map = Graph()
+    
+    # load user query graph
+    # load map
+    initParse()
+    g_query = fetchGraph()
+    g_map = fetchGraph() #???
+    
+    
+    # find the most suitable starting vertex
+    edges = []
+    for i, edge in enumerate(g_query.es):
+        class_e = Edge(edge.index, edge["length"], edge["n1"], edge["n2"], edge["bearing1"], edge["bearing2"])
+        edges.append(class_e)
+    angleArray = getAngleArrayWithEdgeArray(edges)      
     ranked_ID_Angle = rankAnglesAccordingToDistanceFromRightAngle(angleArray)
-	
-	query_start_ID = ranked_ID_Angle[0][0]
-	queue_query = []
-	queue_map = []
-	for id in enumerate(g_query.vs)
-	    queue_query.append((query_start_ID+id) % len(g_query.vs))
-	ISO(queue_query, queue_map, g_query, g_map)
+    
+    query_start_ID = ranked_ID_Angle[0][0]
+    queue_query = []
+    queue_map = []
+    for id in enumerate(g_query.vs)
+        queue_query.append((query_start_ID+id) % len(g_query.vs))
+    ISO(queue_query, queue_map, g_query, g_map)
     
 
 
-def ISO( queue_query, queue_map, g_query, g_map ): 						# 2,4
-	candidates = find_neighbor(queue_query, queue_map, g_query, g_map) 			# 2,4 [5 or 6]
-	
-	for i in candidates				# 5,6
-		queue_map.append(i)					# 2,4,5
-        if len(queue_map) == len(queue_query)	
-		    iso_subgraph = []
+def ISO( queue_query, queue_map, g_query, g_map ):                      # 2,4
+    candidates = find_neighbor(queue_query, queue_map, g_query, g_map)          # 2,4 [5 or 6]
+    
+    for i in candidates             # 5,6
+        queue_map.append(i)                 # 2,4,5
+        if len(queue_map) == len(queue_query)   
+            iso_subgraph = []
             for vid in queue_map
-			    iso_subgraph.append((g_map.vs[vid]["lat"],g_map.vs[vid]["lng"]))
-			# output iso_subgraph
-		else
-		    to_be_checked_edgeID = g_map.get_eid(queue_map[len(queue_map)], queue_map[len(queue_map)-1])
-			g_map_copy = g_map.copy()
-			g_map_copy.es[to_be_checked_edgeID]["checked"] = True
-			ISO(queue_query, queue_map, g_query, g_map_copy) 					# 2,4,5
-		# queue.pop()					
-	
-	
+                iso_subgraph.append((g_map.vs[vid]["lat"],g_map.vs[vid]["lng"]))
+            # output iso_subgraph
+        else
+            to_be_checked_edgeID = g_map.get_eid(queue_map[len(queue_map)], queue_map[len(queue_map)-1])
+            g_map_copy = g_map.copy()
+            g_map_copy.es[to_be_checked_edgeID]["checked"] = True
+            ISO(queue_query, queue_map, g_query, g_map_copy)                    # 2,4,5
+        # queue.pop()                   
+    
+    
--- graph.py
+++ graph.py
@@ -20,20 +20,20 @@ def fetchGraph():
     for node in nodes:
         v = g.vs[nodes.index(node)]
         v["checked"] = False
-		v["nid"] = str(nodes.index(node))
-		v["lat"] = node.lat
-		v["lng"] = node.lng
-		
+        v["nid"] = str(nodes.index(node))
+        v["lat"] = node.lat
+        v["lng"] = node.lng
+        
     for i, edge in enumerate(edges):
         v1 = g.vs[nodes.index(edge.node1)]
         v2 = g.vs[nodes.index(edge.node2)]
         g.add_edges(v1,v2)
         g.es[i]["n"] = {v1["nid"]:edge.bearing1, v2["nid"]:edge.bearing2}
         g.es[i]["length"] = edge.length
-		g.es[i]["n1"] = v1["nid"]
-		g.es[i]["n2"] = v2["nid"]
-		g.es[i]["bearing1"] = edge.bearing1
-		g.es[i]["bearing2"] = edge.bearing2       
+        g.es[i]["n1"] = v1["nid"]
+        g.es[i]["n2"] = v2["nid"]
+        g.es[i]["bearing1"] = edge.bearing1
+        g.es[i]["bearing2"] = edge.bearing2       
         g.es[i]["checked"] = False
 
 
